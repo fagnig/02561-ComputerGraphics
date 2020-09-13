eed23da683143f7b2e37f3159034b7c84e1f5481
fagnig <fagnig@autistklassen.dk>
2020-09-13 20:24:55 +0200

    finished wks3(2019ed)

--- wks3/wks3_2.js
+++ wks3/wks3_2.js
@@ -15,15 +15,31 @@ var numPoints = [ vec3(0.0, 0.0, 0.0), vec3(1.0, 0.0, 0.0),
                   vec3(1.0, 1.0, 0.0), vec3(1.0, 1.0, 1.0),
                 ]
 
-var radius = 0.8;
+var radius = 5;
 var theta  = radians(45.0);
 var phi    = radians(45.0);
+var znear  = -3.0
+var zfar   = 1.0
+var fov    = 45.0
 
-var eye = vec3(radius*Math.sin(phi), radius*Math.sin(theta), radius*Math.cos(phi));
-var at  = vec3(0.0, 0.0, 0.0);
+var at  = vec3(0.5, 0.5, 0.5);
 var up  = vec3(0.0, 1.0, 0.0);
 
-var altperspective = false;
+var perspectivetype = 0;
+
+function vec3_getEye(type) {
+  if (type == 0) {
+    return vec3(at[0], at[1], radius*Math.cos(theta))
+  } else if (type == 1) {
+    return vec3(radius*Math.cos(theta)*Math.cos(phi), at[1], radius*Math.cos(theta))
+  } else if (type == 2) {
+    return vec3(radius*Math.sin(theta)*Math.cos(phi), radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta))
+  }
+}
+
+function mat4_getModelViewMatrix(eye) {
+  return lookAt(eye, at, up);
+}
 
 window.onload = function()
 {
@@ -34,6 +50,7 @@ window.onload = function()
 
   gl.viewport(0, 0, canv.width, canv.height);
   gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
+  gl.enable(gl.DEPTH_TEST)
 
   program = initShaders(gl, "vertex-shader", "fragment-shader")
   gl.useProgram(program);
@@ -46,10 +63,14 @@ window.onload = function()
   gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(vPosition);
 
-  var modelViewMatrix = lookAt(eye, at, up);
-
   var viewloc = gl.getUniformLocation(program, "view");
-  gl.uniformMatrix4fv(viewloc, false, flatten(modelViewMatrix) );
+  gl.uniformMatrix4fv(viewloc, false, flatten(mat4_getModelViewMatrix(vec3_getEye(perspectivetype))) );
+
+  var aratio = canv.width / canv.height;
+  var projMat = perspective(fov, aratio, znear, zfar);
+
+  var projloc = gl.getUniformLocation(program, "proj");
+  gl.uniformMatrix4fv(projloc, false, flatten(projMat) );
 
   render(buffer)
 }
@@ -66,17 +87,10 @@ function doKeyDown(e)
 {
   if(e.keyCode == 32)
     {
-      altperspective = !altperspective;
-      if(altperspective){
-        theta = radians(35.0);
-        phi   = radians(50.0);
-      } else {
-        theta = radians(45.0);
-        phi   = radians(45.0);
-      }
+      perspectivetype = (++perspectivetype) % 3;
+      document.getElementById("perspectiveinfo").innerHTML = "Current perspective: "+(perspectivetype+1)+"-point";
 
-      eye = vec3(radius*Math.sin(phi), radius*Math.sin(theta), radius*Math.cos(phi));
-      var modelViewMatrix = lookAt(eye, at, up);
+      var modelViewMatrix = flatten(mat4_getModelViewMatrix(vec3_getEye(perspectivetype)));
       var viewloc = gl.getUniformLocation(program, "view");
       gl.uniformMatrix4fv(viewloc, false, flatten(modelViewMatrix) );    
     }
